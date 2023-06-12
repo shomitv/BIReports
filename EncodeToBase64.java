c6ae38d0eb8572d7dcc01656f83fa1f895472c76
shomitv <shomit93@gmail.com>
2023-06-12 12:31:46 +0530

    java update

--- EncodeToBase64.java
+++ EncodeToBase64.java
@@ -14,7 +14,7 @@ public static void main(String[] args){
     Base64.Encoder encoder = Base64.getEncoder();
     //System.out.println( encoder.encodeToString(array));
 
-    String str = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:v2=\"http://xmlns.oracle.com/oxp/service/v2\">\r\n   <soapenv:Header/>\r\n   <soapenv:Body>\r\n      <v2:uploadObject>\r\n         <v2:reportObjectAbsolutePathURL>/Custom/Chandu</v2:reportObjectAbsolutePathURL>\r\n         <v2:objectType>xdm</v2:objectType>\r\n         <v2:objectZippedData>"+encoder.encodeToString(array)+"</v2:objectZippedData>\r\n         <v2:userID>lisa.jones</v2:userID>\r\n         <v2:password>fUX3t#7?</v2:password>\r\n      </v2:uploadObject>\r\n   </soapenv:Body>\r\n</soapenv:Envelope>";
+    String str = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:v2=\"http://xmlns.oracle.com/oxp/service/v2\">\r\n   <soapenv:Header/>\r\n   <soapenv:Body>\r\n      <v2:uploadObject>\r\n         <v2:reportObjectAbsolutePathURL>"+args[2]+"</v2:reportObjectAbsolutePathURL>\r\n         <v2:objectType>xdmz</v2:objectType>\r\n         <v2:objectZippedData>"+encoder.encodeToString(array)+"</v2:objectZippedData>\r\n         <v2:userID>"+args[3]+"</v2:userID>\r\n         <v2:password>"+args[4]+"</v2:password>\r\n      </v2:uploadObject>\r\n   </soapenv:Body>\r\n</soapenv:Envelope>";
     BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]));
     writer.write(str);
      writer.close();
